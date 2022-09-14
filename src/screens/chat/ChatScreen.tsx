324a5dd0c3235ce44399893097b11505629cf9f6
umar <umar.dev500@gmail.com>
2022-09-15 00:17:24 +0700

    remove loaded check

--- src/screens/chat/ChatScreen.tsx
+++ src/screens/chat/ChatScreen.tsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ImageBackground } from 'react-native';
 import { Col } from '../../components/atoms';
 import { ChatForm, ChatHeader, ChatList } from '../../components/organisms';
 
 export const ChatScreen: React.FC = () => {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoaded(true);
-    }, 50);
-  }, []);
-
   return (
     <Col flex={1} bg="white">
       <ChatHeader />
@@ -19,12 +11,8 @@ export const ChatScreen: React.FC = () => {
         style={{ flex: 1 }}
         source={require('../../../assets/images/background/wa-bg.png')}
       >
-        {loaded ? (
-          <>
-            <ChatList />
-            <ChatForm />
-          </>
-        ) : null}
+        <ChatList />
+        <ChatForm />
       </ImageBackground>
     </Col>
   );
